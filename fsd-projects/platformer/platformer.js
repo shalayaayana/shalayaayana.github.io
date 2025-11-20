b199df33080d3e54b6079212f16dd2761544bce5
shalayaayana <shalaya.george@inspire35.org>
2025-11-20 16:05:08 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,11 +27,11 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     
 
 
     // TODO 2 - Create Platforms
-
+    createPlatform()
 
 
 
